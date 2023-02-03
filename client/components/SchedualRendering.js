e6b6be82edcb5b41824a32a3b90e1b2f33d87b60
isOdin <44323275+Proshnick2016@users.noreply.github.com>
2023-02-03 16:23:32 +0000

    1weekAnd3DaysBeforeDeadLine

--- client/components/SchedualRendering.js
+++ client/components/SchedualRendering.js
@@ -1,9 +1,5 @@
 import React from "react";
-<<<<<<< HEAD
 import { Text, FlatList, AsyncStorage, SafeAreaView } from "react-native";
-=======
-import { Text, FlatList, AsyncStorage, SafeAreaView, View } from "react-native";
->>>>>>> aace6070b8f123c1d7cc19191c72631d110f0f9b
 import schedualStyles from "../styles/schedualStyles";
 
 export default function SchedualRendering({ weekDayName }) {
@@ -22,20 +18,11 @@ export default function SchedualRendering({ weekDayName }) {
     });
 
     return (
-<<<<<<< HEAD
         <SafeAreaView>
             <Text style={schedualStyles.weekDayName}>{weekDayName}</Text>
             <FlatList data={weekDayArray} renderItem={({ item }) =>
                 <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
             }
-=======
-        <SafeAreaView style={schedualStyles.container}>
-            <Text style={schedualStyles.weekDayName}>{weekDayName}</Text>
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>}
->>>>>>> aace6070b8f123c1d7cc19191c72631d110f0f9b
             />
         </SafeAreaView>
     );
--- client/pages/Schedual.js
+++ client/pages/Schedual.js
@@ -1,31 +1,10 @@
 import React, { useState } from "react";
-<<<<<<< HEAD
 import { View, ScrollView } from "react-native";
 import SchedualRendering from "../components/SchedualRendering";
-=======
-import { SafeAreaView, View, Text, FlatList, AsyncStorage } from "react-native";
-import SchedualRendering from "../components/SchedualRendering";
-import schedualPageStyles from "../styles/schedualPageStyles"
->>>>>>> aace6070b8f123c1d7cc19191c72631d110f0f9b
 import schedualStyles from "../styles/schedualStyles";
 
 export default function Schedual() {
-    const [weekDayArray, setWeekDayArray] = React.useState();
-    const [geted, setGeted] = React.useState(false)
-
-    React.useEffect(() => {
-        async function getAsyncStorageData() {
-            const schedualFromAsyncStorage = await AsyncStorage.getItem('Schedual');
-            setWeekDayArray(JSON.parse(schedualFromAsyncStorage)['Понедельник']);
-            setGeted(true)
-        }
-        if (geted === false) {
-            getAsyncStorageData();
-        }
-    });
-
     return (
-<<<<<<< HEAD
         <View>
             <ScrollView>
                 <View style={schedualStyles.item}>
@@ -47,48 +26,9 @@ export default function Schedual() {
         </View>
     );
 }
-=======
-        <View style={schedualStyles.container}>
-            <Text style={schedualStyles.weekDayName}>Понедельник</Text>
-
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>
-            }
-            />
-            <Text style={schedualStyles.weekDayName}>Понедельник</Text>
-
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>
-            }
-            /><Text style={schedualStyles.weekDayName}>Понедельник</Text>
-
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>
-            }
-            /><Text style={schedualStyles.weekDayName}>Понедельник</Text>
-
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>
-            }
-            /><Text style={schedualStyles.weekDayName}>Понедельник</Text>
-
-            <FlatList data={weekDayArray} renderItem={({ item }) =>
-                <View style={schedualStyles.item}>
-                    <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
-                </View>
-            }
-            />
-
-        </View>
-    );
-}
 // style={schedualStyles.item}
->>>>>>> aace6070b8f123c1d7cc19191c72631d110f0f9b
+/*
+<FlatList data={weekDayArray} renderItem={({ item }) =>
+                        <Text> {item.lessonNumber} {item.lessonName} {item.lessonTime}</Text>
+                    }
+*/
